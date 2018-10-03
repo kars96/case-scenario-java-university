1163aa3a27d4575dd8709db0df406ff0b8ee5908
724408 <724408@PC251171.cts.com>
2018-10-03 10:41:29 +0530

    third

---
+++ src/com/History.java
@@ -0,0 +1,15 @@
+package com;
+
+public class History extends Subject{
+
+	public History(int totalMarks) {
+		super(totalMarks);
+		// TODO Auto-generated constructor stub
+	}
+
+	public History(String desc, int totalMarks) {
+		super(desc, totalMarks);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
